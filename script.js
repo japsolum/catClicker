987f1ff4f94e885191042c7f8d9b56116cbe2a59
japsolum <japsolum@gmail.com>
2017-09-23 19:58:28 -0600

    Redo
    Re wrote catClicker app using knockout js

--- script.js
+++ script.js
@@ -1,17 +1,87 @@
-var numOfClicksTiger=0,
-	numOfClicksLeopard=0,
-	image1Name = "Tiger",
-	image2Name = "Leopard";
+const initialCats= {
+	"cats" : [
+		{
+			"name" : "Tiger",
+			"imgSrc" : "images/tiger.jpg",
+			"nicknames" : ["Striped killer"],
+			"clickCount" : 0
+		},
 
-document.getElementById("subHeader1").innerHTML=image1NameName;
-document.getElementById("subHeader2").innerHTML=image2Name;
+		{
+			"name" : "Leopard",
+			"imgSrc" : "images/leopard.jpg",
+			"nicknames" : ["Spotted assassin"],
+			"clickCount" : 0
+		},
 
-$("#tiger").click(function() {
-	numOfClicksTiger += 1;
-    document.getElementById("numClicksTiger").innerHTML=numOfClicksTiger;
-});
+		{
+			"name" : "Cheetah",
+			"imgSrc" : "images/cheetah.jpg",
+			"nicknames" : ["Speedster"],
+			"clickCount" : 0
+		},
 
-$("#leopard").click(function() {
-	numOfClicksLeopard += 1;
-    document.getElementById("numClicksLeopard").innerHTML=numOfClicksLeopard;
-});
+		{
+			"name" : "Panther",
+			"imgSrc" : "images/panther.jpg",
+			"nicknames" : ["Shadow"],
+			"clickCount" : 0
+		},
+
+		{
+			"name" : "Lynx",
+			"imgSrc" : "images/lynx.jpg",
+			"nicknames" : ["Snow Cat"],
+			"clickCount" : 0
+		}
+	]
+};
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.nicknames = ko.observableArray(data.nicknames)
+
+	this.level = ko.computed(function() {
+		var clickCount = this.clickCount(),
+			level;
+
+		if (clickCount < 10) {
+			return("Newborn");
+		} else if (clickCount < 50) {
+			return("Infant");
+		} else if (clickCount < 100) {
+			return("Child");
+		} else if (clickCount < 200) {
+			return("Teen");
+		} else if (clickCount < 500) {
+			return("adult");
+		} else {
+			return("Ninja");
+		}
+
+	}, this);
+};
+
+var ViewModel = function() {
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.cats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+	this.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+	};
+
+	this.setCat = function(clickedCat) {
+		self.currentCat(clickedCat);
+	};
+}
+
+ko.applyBindings(new ViewModel);
+
